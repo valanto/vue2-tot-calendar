193fed25f004db88ad9d4567eecb4931de5e56ee
Chrysovalanto Kousetti <valanto@suite5.eu>
2021-09-30 22:16:43 +0300

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import Vue2TotCalendar from '@/vue2-tot-calendar.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Vue2TotCalendar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-tot-calendar />
+  </div>
+</template>
